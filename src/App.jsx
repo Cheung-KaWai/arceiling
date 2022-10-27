d202f90f26c5876e28a19b8fcaf844207d82d358
Kawai cheung <kawaicheungdev@gmail.com>
2022-10-27 09:44:11 +0200

    commit ar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import "./App.css";
 import { isAndroid, isIOS } from "react-device-detect";
+import glbFile from "./assets/tal.glb";
+import usdz from "./assets/test.usdz";
+import image from "./assets/tal.jpg";
+import styled from "styled-components";
 
 function App() {
   const renderContent = () => {
     if (isAndroid) {
-      return <p>Hello Android</p>;
+      return (
+        <>
+          <p>Hello Android</p>;
+          <model-viewer
+            src={glbFile}
+            ar
+            ar-modes="webxr"
+            camera-controls
+            poster={image}
+            shadow-intensity="1"
+          ></model-viewer>
+        </>
+      );
     }
     if (isIOS) {
-      return <p>Hello Iphone</p>;
+      return (
+        <>
+          <p>Hello Iphone</p>
+          <a rel="ar" href={usdz}>
+            <ImgStyled src={image} />
+          </a>
+        </>
+      );
     }
     return <p>not detected</p>;
   };
@@ -21,3 +44,8 @@ function App() {
 }
 
 export default App;
+
+const ImgStyled = styled.img`
+  width: 100%;
+  height: auto;
+`;
