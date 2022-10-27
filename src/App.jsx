9e985751b0ffd60655e52b388df90350cf7bfff7
Kawai cheung <kawaicheungdev@gmail.com>
2022-10-27 09:34:39 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import "./App.css";
 import { isAndroid, isIOS } from "react-device-detect";
 
 function App() {
+  const renderContent = () => {
+    if (isAndroid) {
+      return <p>Hello Android</p>;
+    }
+    if (isIOS) {
+      return <p>Hello Iphone</p>;
+    }
+    return <p>not detected</p>;
+  };
+
   return (
     <div>
-      {isAndroid ?? <h1>android</h1>}
-      {isIOS ?? <h1>ios</h1>}
+      <h1>Test</h1>
+      {renderContent()}
     </div>
   );
 }
