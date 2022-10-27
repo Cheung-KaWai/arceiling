41d047dc6c28dce7c1330ace533d880bba2b66e4
Kawai cheung <kawaicheungdev@gmail.com>
2022-10-27 09:49:45 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,17 @@ function App() {
       return (
         <>
           <p>Hello Android</p>
-          <model-viewer
-            src={glbFile}
-            ar
-            ar-modes="webxr"
-            camera-controls
-            // poster={image}
-            shadow-intensity="1"
-          ></model-viewer>
+          <model-viewer src={glbFile} ar ar-modes="webxr" camera-controls poster="poster.webp" shadow-intensity="1">
+            <div class="progress-bar hide" slot="progress-bar">
+              <div class="update-bar"></div>
+            </div>
+            <button slot="ar-button" id="ar-button">
+              View in your space
+            </button>
+            <div id="ar-prompt">
+              <img src="https://modelviewer.dev/shared-assets/icons/hand.png" />
+            </div>
+          </model-viewer>
         </>
       );
     }
